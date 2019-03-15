a4114e52d2f6f0a47314bba42f327ef16b48bb32
ICEJJ <lpc0503@gmail.com>
2019-03-15 21:32:04 +0800

    [src] testing [Declaration] every declaration should be here [const] we can declare CONSTANT using this

---
+++ src/Declaration.py
@@ -0,0 +1,6 @@
+import pygame as py
+import time
+import random
+import const
+
+const.PI = 3.14
---
+++ src/src.py
@@ -0,0 +1,11 @@
+from Declaration import *
+
+
+def game_loop():
+	
+	print(2*const.PI)
+	print(random.randint(0, 100))
+
+if __name__ == "__main__":
+	py.init()
+	game_loop()
