565bcb8ce97884256a6fb5472e08dd0f34e54295
SunTalk <infernal56727@gmail.com>
2019-03-18 18:58:59 +0800

    board is working

--- src/board.py
+++ src/board.py
@@ -46,28 +46,20 @@ def board_build() :
 	bound(board)
 	for i in range( 0 ,len(obstacle_x) ) :
 		block(board,board_x[obstacle_x[i]][obstacle_y[i]],board_y[obstacle_x[i]][obstacle_y[i]],60,cyan_blue)
-
-obstacle_x = []
-obstacle_y = []
-
-def add_obstacle(x,y) :
-	obstacle_x.append(x)
-	obstacle_y.append(y)
-
-def clean_obstacle() :
-	obstacle_x.clear()
-	obstacle_y.clear()
-	board_build()
+	for i in range( 0 ,len(solider_x) ) :
+		block(board,board_x[solider_x[i]][solider_y[i]],board_y[solider_x[i]][solider_y[i]],60,red)
 
 board_angle = 0
 board_change_angle = 0
 action = True
 board_center_pos = (400,400)
+board_mode = 0
 
 def board_display(surface):
 	global board_change_angle
 	global board_angle
 	global action
+	global move
 	if board_change_angle > 0 :
 		board_angle += 2
 		board_change_angle -=2
@@ -86,38 +78,74 @@ def board_display(surface):
 
 def board_action(event):
 	global board_change_angle
+	global board_mode
 	if event.type == py.KEYDOWN:
 		if event.key == py.K_RIGHT:
 			if action :
 				board_change_angle = -90
+				board_mode += 1
+				board_mode = board_mode%4
 				print('right')
 		if event.key == py.K_LEFT:
 			if action :
 				board_change_angle = 90
+				board_mode -= 1
+				board_mode = board_mode%4
 				print('left')
 		if event.key == py.K_DOWN :
-			clean_obstacle()
 			print('down')
+		if event.key == 'q' :
+			add_obstacle(B5_x,B5_y)
+			add_solider(A1_x,A1_y)
+			board_build()
 
 ## end board_action
 
 ## end board
 
-## for test
-add_obstacle(B5_x,B5_y)
-board_build()
-while const.GAME_LOOP:
-	
-	for event in py.event.get():
-		if event.type == py.QUIT:
-			py.quit()
-			quit()
-		board_action(event)
+## chess
 
-	display.fill(black)
-	board_display(display)
-	
-	py.display.update()
-	clock.tick(60)
+obstacle_x = []
+obstacle_y = []
 
-## end test
+def add_obstacle(x,y) :
+	obstacle_x.append(x)
+	obstacle_y.append(y)
+
+def clear_obstacle() :
+	obstacle_x.clear()
+	obstacle_y.clear()
+	board_build()
+
+solider_x = []
+solider_y = []
+
+def add_solider(x,y) :
+	solider_x.append(x)
+	solider_y.append(y)
+
+def clear_solider() :
+	solider_x.clear()
+	solider_y.clear()
+	board_build()
+
+move = False
+def solider_down() :
+	if move :
+		for i in range(0,len(solider_y)) :
+			if board_mode == 0 :
+				if solider_x[i] < 7 :
+					solider_x[i] += 1
+			if board_mode == 1 :
+				if solider_y[i] < 7 :
+					solider_y[i] += 1
+			if board_mode == 2 :
+				if solider_x[i] > 0 :
+					solider_x[i] -= 1
+			if board_mode == 3 :
+				if solider_y[i] > 0 :
+					solider_y[i] -= 1
+		py.time.delay(50)
+
+## end chess
+
---
+++ src/board_test.py
@@ -0,0 +1,23 @@
+from board import *
+
+py.init()
+
+add_obstacle(B5_x,B5_y)
+add_solider(A1_x,A1_y)
+board_build()
+while const.GAME_LOOP:
+
+	for event in py.event.get():
+		if event.type == py.QUIT:
+			py.quit()
+			quit()
+		board_action(event)
+
+	display.fill(black)
+	board_display(display)
+
+	solider_down()
+	board_build()
+	
+	py.display.update()
+	clock.tick(60)
