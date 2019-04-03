fdacbf9fa8f759a3d7a7f1eb0260615d8fd6e462
SunTalk <infernal56727@gmail.com>
2019-04-03 16:16:03 +0800

    fix bug

--- src/level_four.py
+++ src/level_four.py
@@ -81,7 +81,7 @@ def level_four_run(level_four_board,level_four_surface) :
 		
 	obstacle_list[0].defeat(level_four_board.get_spin(),level_four_board.get_mode(),level_four_board.get_start_move(),soldier_list,enemy_list)
 
-	if level_four_board.get_spin() == False and level_four_board.get_move()%3 == 0 and level_four_board.get_clock() == 1 :
+	if level_four_board.get_spin() == False and level_four_board.get_move()%3 == 0 and level_four_board.get_clock() == 1 and len(enemy_list) != 0 :
 		enemy_list.append(chess( random.randint(0,7), random.randint(0,7), const.ENEMY, random.randint(1,3) ))
 		enemy_list.append(chess( random.randint(0,7), random.randint(0,7), const.ENEMY, random.randint(1,3) ))
 		enemy_list.append(chess( random.randint(0,7), random.randint(0,7), const.ENEMY, random.randint(1,3) ))
--- src/src.py
+++ src/src.py
@@ -65,8 +65,11 @@ def clear_WORLD():
 	global CHAPTER
 	global ACT
 
-	WORLD_LINE = 'N'
-	CHAPTER   = const.CHAPTER_1
+	# WORLD_LINE = 'N'
+	# CHAPTER   = const.CHAPTER_1
+	# ACT       = const.ACT_1
+	WORLD_LINE = 'Z'
+	CHAPTER   = const.CHAPTER_3
 	ACT       = const.ACT_1
 
 def init():
