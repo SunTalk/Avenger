3c21e795437227d876da83353f870b69353b12f9
SunTalk <infernal56727@gmail.com>
2019-04-10 09:53:53 +0800

    update

--- src/src.py
+++ src/src.py
@@ -25,7 +25,6 @@ max_move    = textHandler()
 
 menu    = interface()
 info    = interface()
-dplayer = interface()
 plot    = interface()
 game    = interface()
 finish  = interface()
@@ -58,19 +57,8 @@ def initButton():
 						   "INFO",
 						   128
 						   )
-	menu.set_custom_button(const.MENU_START_BUTTON_X*4,
-						   const.MENU_START_BUTTON_Y,
-						   const.MENU_START_BUTTON_WIDTH,
-						   const.MENU_START_BUTTON_HEIGHT,
-						   white,
-						   "2PLAYER",
-						   128
-						  )
 
 	info.set_button(const.INFO)
-
-	dplayer.set_custom_button(1000, 300, 200, 100, white, "restart", 128)
-	dplayer.set_custom_button(1000, 600, 200, 100, white, "menu", 128)
 
 	plot.set_custom_button(1000, 0, 200, 100, white, "SKIP", 128)
 	game.set_custom_button(1000, 300, 200, 100, white, "restart", 128)
@@ -210,8 +198,8 @@ def loadMUSIC(name):
 				soundHandler.stop()
 			soundHandler.loadMUSIC(name)
 			py.time.delay(500)
-		# else:
-		# 	print("music is same")
+		else:
+			print("music is same")
 
 def play_music():
 	if music:
@@ -251,7 +239,7 @@ def update(class_object, class_object2=None, write_object=None, board=None):
 			writeMove(board)
 			level_newplayer_run(level_board, level_surface)
 
-	if GAME_STATE == const.DOUBLE:
+	if GAME_STATE == const.DOUBLEPLAYER:
 		if board != None :
 			writeMove(board)
 			random_obstacle(level_board)
@@ -308,16 +296,15 @@ def run_menu():
 		print("start")
 		GAME_STATE = const.LOADING
 		NEXT_STATE = const.PLOT
+		# CHAPTER   = const.CHAPTER_1
+		# ACT       = const.ACT_1
 		clear(menu)
+
 	elif menu.custom_is_press():
 		if menu.get_custom_button_name('INFO'):
 			print('INFO')
 			GAME_STATE = const.LOADING
 			NEXT_STATE = const.INFO
-		if menu.get_custom_button_name('2PLAYER'):
-			print('2PLAYER')
-			#GAME_STATE = const.LOADING
-			#NEXT_STATE = const.DOUBLEPLAYER
 		clear(menu)
 
 	elif menu.back_is_press():
@@ -340,9 +327,6 @@ def run_info():
 			NEXT_STATE = const.MENU
 			clear(info)
 			break
-
-def run_doublePlayer():
-	pass
 
 def run_plot():
 
@@ -631,9 +615,7 @@ switch = {
 	const.GAME_FINISH:
 		run_game_finish,
 	const.LOADING:
-		run_loading,
-	const.DOUBLEPLAYER:
-		run_doublePlayer
+		run_loading
 }
 
 def game_loop():
